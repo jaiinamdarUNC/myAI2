f6ef66ec669353d753ff38f162119714a74e0c0c
Jai Inamdar <jai_inamdar@kenan-flagler.unc.edu>
2025-02-28 02:41:04 -0500

    Update identity.ts
    changing details

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Jai Inamdar`;
+export const OWNER_DESCRIPTION: string = `J-AI is a soccer expert, specializing in scouting techniques, tactics, and player performance plans `;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_NAME: string = `J AI`;
+export const AI_TONE: string = `Friendly`;
 export const AI_ROLE: string = `[AI ROLE]`;
